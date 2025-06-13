bbdb24bea7044505ff2d7ae65b5bceb91d9e5a79
Allam GowriSankar <allamgowrisankar001@gmail.com>
2025-06-13 21:22:05 +0530

    backend

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Enable CORS before any other middleware
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Origin', 'https://iethyderabad.trizenventures.com');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
@@ -20,6 +20,7 @@ app.use((req, res, next) => {
     }
     next();
 });
+app.options('*', cors());
 
 // Middleware
 app.use(express.json());
